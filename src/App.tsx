854f94a9f092ef3f9ea1fc68b4d6fefddd172815
VICTUS <giabaodongthanh@gmail.com>
2026-06-30 22:53:32 +0700

    add scroll

--- src/App.tsx
+++ src/App.tsx
@@ -351,7 +351,7 @@ export default function App() {
   const isAudioPlaying = roomState.audioState?.isPlaying || false;
 
   return (
-    <div className="min-h-screen lg:h-screen lg:max-h-screen bg-[#050508] text-slate-100 flex flex-col font-sans selection:bg-indigo-500/30 overflow-x-hidden lg:overflow-hidden relative">
+    <div className="min-h-screen bg-[#050508] text-slate-100 flex flex-col font-sans selection:bg-indigo-500/30 overflow-x-hidden relative">
       {/* Immersive Background Radial Gradients & Glow overlays */}
       <div className="absolute inset-0 overflow-hidden pointer-events-none z-0">
         <div className="absolute inset-0 bg-[radial-gradient(circle_at_50%_50%,_#1a1a2e_0%,_#050508_100%)]" />
@@ -423,10 +423,10 @@ export default function App() {
       </header>
 
       {/* MAIN SCREEN GRID */}
-      <main className="flex-1 max-w-7xl w-full mx-auto p-4 lg:p-6 grid grid-cols-1 lg:grid-cols-3 gap-6 overflow-hidden lg:overflow-hidden min-h-0 lg:max-h-[calc(100vh-140px)] relative z-10">
+      <main className="flex-1 max-w-7xl w-full mx-auto p-4 lg:p-6 grid grid-cols-1 lg:grid-cols-3 gap-6 relative z-10">
         
         {/* LEFT / CENTER COLUMN: Video Meet grid + Audio synchronizer */}
-        <div className="lg:col-span-2 flex flex-col gap-6 overflow-y-auto pr-0 lg:pr-1 min-h-0">
+        <div className="lg:col-span-2 flex flex-col gap-6">
           
           {/* Active Error Notice */}
           {error && (
@@ -483,7 +483,7 @@ export default function App() {
         </div>
 
         {/* RIGHT COLUMN: Chat Room & Members Panel */}
-        <div className="hidden lg:block lg:col-span-1 h-full min-h-0">
+        <div className="hidden lg:block lg:col-span-1 lg:h-[650px] lg:sticky lg:top-6">
           <ChatPanel room={roomState} userId={userId} onSendMessage={handleSendMessage} />
         </div>
       </main>
